8c4f706d305a72f1d642cb68b0992f8c254f9965
zakharych <alexzlkl@yandex.ru>
2020-12-19 17:16:29 +0300

    feat: button to chose params

--- src/js/script.js
+++ src/js/script.js
@@ -5,8 +5,7 @@ import 'normalize.css';
 
 const searchInput = document.getElementById('search');
 const results = document.getElementById('results');
-const newCases = document.getElementById('NewConfirmed');
-const totalCases = document.getElementById('TotalConfirmed');
+const buttonsTotal = document.querySelector('.buttonsTotal');
 
 let searchTerm = '';
 let countriesList;
@@ -18,18 +17,19 @@ const fetchCountries = async () => {
   ).then((res) => res.json());
 };
 const fetchFlag = async (CountryCode) => {
-  const link = `https://restcountries.eu/rest/v2/alpha/${CountryCode}?fields=name;flag`;
+  const link = `https://restcountries.eu/rest/v2/alpha/${CountryCode}?fields=population;flag`;
   flag = await fetch(link).then((res) => res.json());
 };
 
 function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return x.toFixed().toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
 const showFlag = async (CountryCode) => {
   await fetchFlag(CountryCode);
   const id = `#${CountryCode}`;
   const countryFlag = document.createElement('img');
+  countryFlag.innerHTML = '';
   const li = document.querySelector(id);
 
   countryFlag.src = flag.flag;
@@ -37,65 +37,83 @@ const showFlag = async (CountryCode) => {
   li.prepend(countryFlag);
 };
 
-const showCountries = async (param) => {
+const getParams = async (CountryCode, data, population, filterParam) => {
+  await fetchFlag(CountryCode);
+  const id = `#${CountryCode}`;
+  const countryInfo = document.createElement('div');
+  const countryPopulation = document.createElement('h2');
+  const countryPopulationText = document.createElement('h5');
+  const li = document.querySelector(id);
+
+  let nameParam = filterParam.split(/(?=[A-Z])/).join(' ');
+  if (population) {
+    countryPopulation.innerText = numberWithCommas((data / flag.population) * 100000);
+    nameParam += ' on 100k';
+  } else {
+    countryPopulation.innerText = numberWithCommas(data);
+  }
+  countryPopulation.classList.add('country-item__population');
+  countryInfo.classList.add('country-item__info');
+
+  countryPopulationText.innerText = nameParam;
+  countryPopulationText.classList.add('country-item__population--text');
+
+  countryInfo.appendChild(countryPopulation);
+  countryInfo.appendChild(countryPopulationText);
+
+  li.appendChild(countryInfo);
+};
+
+function createListElement(country, filterParam, ul, population) {
+  const data = country[`${filterParam}`];
+  const li = document.createElement('li');
+  const countryName = document.createElement('h3');
+
+  li.classList.add('country-item');
+  li.id = country.CountryCode;
+
+  countryName.innerText = country.Country;
+  countryName.classList.add('country-item__name');
+
+  getParams(country.CountryCode, data, population, filterParam);
+
+  li.appendChild(countryName);
+  ul.appendChild(li);
+  showFlag(country.CountryCode);
+}
+
+const showCountries = async (filterParam, population) => {
   results.innerHTML = '';
-  console.log(param);
 
   // getting the data
   await fetchCountries();
-  // creating structure
 
+  // creating structure
   const ul = document.createElement('ul');
   ul.classList.add('countries');
-  console.log(countriesList);
   countriesList.Countries
     .filter((country) => country.Country.toLowerCase().includes(searchTerm.toLowerCase()))
-    .sort((a, b) => a.TotalConfirmed - b.TotalConfirmed)
+    .sort((a, b) => a[`${filterParam}`] - b[`${filterParam}`])
     .reverse()
     .forEach((country) => {
-      console.log(country);
-      const li = document.createElement('li');
-      const countryName = document.createElement('h3');
-      const countryInfo = document.createElement('div');
-      const countryPopulation = document.createElement('h2');
-      const countryPopulationText = document.createElement('h5');
-
-      li.classList.add('country-item');
-      li.id = country.CountryCode;
-      countryInfo.classList.add('country-item__info');
-
-      countryName.innerText = country.Country;
-      countryName.classList.add('country-item__name');
-
-      countryPopulation.innerText = numberWithCommas(country.TotalConfirmed);
-      countryPopulation.classList.add('country-item__population');
-
-      countryPopulationText.innerText = 'Total Confirmed';
-      countryPopulationText.classList.add('country-item__population--text');
-
-      countryInfo.appendChild(countryPopulation);
-      countryInfo.appendChild(countryPopulationText);
-
-      li.appendChild(countryName);
-      li.appendChild(countryInfo);
-      ul.appendChild(li);
-      showFlag(country.CountryCode);
+      createListElement(country, filterParam, ul, population);
     });
   results.appendChild(ul);
 };
 
 showCountries('TotalConfirmed');
+let timerId = '';
+let currentParam = 'TotalConfirmed';
+let currentAtr = 'TotalConfirmed';
 
 searchInput.addEventListener('input', (e) => {
   searchTerm = e.target.value;
-  showCountries();
+  clearTimeout(timerId);
+  timerId = setTimeout(showCountries, 700, currentParam, currentAtr);
 });
 
-newCases.addEventListener('click', () => {
-  showCountries(newCases.id);
-  console.log();
-});
-
-totalCases.addEventListener('click', () => {
-  showCountries(totalCases.id);
+buttonsTotal.addEventListener('click', (event) => {
+  showCountries(event.target.id, event.target.getAttribute('population'));
+  currentParam = event.target.id;
+  currentAtr = event.target.getAttribute('population');
 });
