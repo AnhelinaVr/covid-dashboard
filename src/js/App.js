3d3ccce8cbccf82e4d83f82894295b7c246f6396
AnhelinaVr <gelina242@gmail.com>
2020-12-20 19:21:06 +0300

    fix: fix switching countries between table and map

--- src/js/App.js
+++ src/js/App.js
@@ -1,26 +1,22 @@
 import Table from './Table';
 import CovidMap from './map';
 
-
 export default class App {
-    constructor() {
-        this.country = { Country: 'Brazil', TotalConfirmed: 7110434 };
-        this.moduleTable = new Table('.module-table', 'module-table__table', this.setCountry.bind(this));
-        this.moduleTable.addTheadandTbody();
-        this.moduleTable.showCountries('Total');
-        this.moduleMap = new CovidMap(document.querySelector('#map'),
-            document.querySelector('#legend'), this.setCountry.bind(this));
-        this.moduleMap.renderData('totalCases');
+  constructor() {
+    this.country = { Country: 'Brazil', TotalConfirmed: 7110434 };
+    this.moduleTable = new Table('.module-table', 'module-table__table', this.setCountry.bind(this));
+    this.moduleTable.addTheadandTbody();
+    this.moduleTable.showCountries('Total');
+    this.moduleMap = new CovidMap(document.querySelector('#map'),
+      document.querySelector('#legend'), this.setCountry.bind(this));
+    this.moduleMap.renderData('totalCases');
+  }
 
-    }
+  /* в свою таблицу передаю функцию которая вызывается когда страна меняется */
 
-    /* в свою таблицу передаю функцию которая вызывается когда страна меняется */
-
-    setCountry(country) {
-        this.country = country;
-        console.log(country, 'app');
-        this.moduleMap.setCountry(country.latlng);
-        this.moduleTable.showDataTargetCountry(country); // ???
-    }
-
+  setCountry(country) {
+    this.country = country;
+    this.moduleMap.setCountry(country.latlng);
+    this.moduleTable.showDataTargetCountry(country.country);
+  }
 }
--- src/js/script.js
+++ src/js/script.js
@@ -4,8 +4,6 @@ import App from './App';
 
 const app = new App();
 
-
-
 // const map = new CovidMap(document.querySelector('#map'),
 //   document.querySelector('#legend'));
 
@@ -13,6 +11,5 @@ const app = new App();
 
 const buttonsContainer = document.querySelector('.map-tabs');
 buttonsContainer.addEventListener('click', (event) => {
-    if (event.target.classList.contains('button-map--tab'))
-        app.moduleMap.renderData(event.target.dataset.tabName);
+  if (event.target.classList.contains('button-map--tab')) { app.moduleMap.renderData(event.target.dataset.tabName); }
 });
